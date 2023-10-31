8a9a93e135a04682694371e0549bc1dd8ac7c984
Stephen <gstephenc@hotmail.com>
2023-10-31 17:10:37 -0400

    added slide change functionality
    - need to add fade animations

--- sliderDesign.js
+++ sliderDesign.js
@@ -1,3 +1,20 @@
-function ChangeSlide(){
-
+function ChangeSlide(direction){
+    const slides = document.querySelectorAll(".slide");
+    let newSlide = 0;
+    for(let i = 0; i < slides.length; i++){
+        if(slides[i].attributes.length == 2){
+            if(i + direction >= slides.length){
+                newSlide = 0;
+            }
+            else if(i + direction < 0){
+                newSlide = slides.length - 1;
+            }
+            else{
+                newSlide = i + direction;
+            }
+            slides[i].removeAttribute("data-active");
+            slides[newSlide].setAttribute("data-active", "");
+            break;
+        };
+    }
 }
